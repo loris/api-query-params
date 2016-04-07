3299870ed4de4d19406048018c569c52af397347
Loris Guignard <loris.guignard@gmail.com>
2016-04-07 10:40:47 +0200

    Migrate deprecated ava 0.14 assert methods

--- test/index.js
+++ test/index.js
@@ -3,32 +3,32 @@ import aqp from '../src';
 
 test('filter: basic', t => {
   const res = aqp('key=value');
-  t.ok(res);
-  t.same(res.filter, { key: 'value' });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: 'value' });
 });
 
 test('filter: numeric casting', t => {
   const res = aqp('key1=10&key2=1.2&key3=0');
-  t.ok(res);
-  t.same(res.filter, { key1: 10, key2: 1.2, key3: 0 });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key1: 10, key2: 1.2, key3: 0 });
 });
 
 test('filter: boolean casting', t => {
   const res = aqp('key1=true&key2=false');
-  t.ok(res);
-  t.same(res.filter, { key1: true, key2: false });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key1: true, key2: false });
 });
 
 test('filter: regex casting', t => {
   const res = aqp('key1=/regex/&key2=/regexi/i');
-  t.ok(res);
-  t.same(res.filter, { key1: /regex/, key2: /regexi/i });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key1: /regex/, key2: /regexi/i });
 });
 
 test('filter: date casting', t => {
   const res = aqp('key1=2016-04&key2=2016-04-12&key3=2016-04-02 08:00');
-  t.ok(res);
-  t.same(res.filter, {
+  t.truthy(res);
+  t.deepEqual(res.filter, {
     key1: new Date('2016-04'),
     key2: new Date('2016-04-12'),
     key3: new Date('2016-04-02 08:00'),
@@ -37,91 +37,91 @@ test('filter: date casting', t => {
 
 test('filter: null casting', t => {
   const res = aqp('key=null');
-  t.ok(res);
-  t.same(res.filter, { key: null });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: null });
 });
 
 test('filter: force casting', t => {
   const res = aqp('key1=string(10)&key2=date(2016)&key3=string(null)');
-  t.ok(res);
-  t.same(res.filter, { key1: '10', key2: new Date('2016'), key3: 'null' });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key1: '10', key2: new Date('2016'), key3: 'null' });
 });
 
 test('filter: $gt operator', t => {
   const res = aqp('key>value');
-  t.ok(res);
-  t.same(res.filter, { key: { $gt: 'value' } });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: { $gt: 'value' } });
 });
 
 test('filter: $lt operator', t => {
   const res = aqp('key<value');
-  t.ok(res);
-  t.same(res.filter, { key: { $lt: 'value' } });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: { $lt: 'value' } });
 });
 
 test('filter: $gte operator', t => {
   const res = aqp('key>=value');
-  t.ok(res);
-  t.same(res.filter, { key: { $gte: 'value' } });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: { $gte: 'value' } });
 });
 
 test('filter: $lte operator', t => {
   const res = aqp('key<=value');
-  t.ok(res);
-  t.same(res.filter, { key: { $lte: 'value' } });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: { $lte: 'value' } });
 });
 
 test('filter: $ne operator', t => {
   const res = aqp('key!=value');
-  t.ok(res);
-  t.same(res.filter, { key: { $ne: 'value' } });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: { $ne: 'value' } });
 });
 
 test('filter: $not operator (with regex)', t => {
   const res = aqp('key!=/value/');
-  t.ok(res);
-  t.same(res.filter, { key: { $not: /value/ } });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: { $not: /value/ } });
 });
 
 test('filter: $in operator (multiple keys)', t => {
   const res = aqp('key=a&key=b');
-  t.ok(res);
-  t.same(res.filter, { key: { $in: ['a', 'b'] } });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: { $in: ['a', 'b'] } });
 });
 
 test('filter: $in operator (comma separated)', t => {
   const res = aqp('key=a,b');
-  t.ok(res);
-  t.same(res.filter, { key: { $in: ['a', 'b'] } });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: { $in: ['a', 'b'] } });
 });
 
 test('filter: $nin operator (multiple keys)', t => {
   const res = aqp('key!=a&key!=b');
-  t.ok(res);
-  t.same(res.filter, { key: { $nin: ['a', 'b'] } });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: { $nin: ['a', 'b'] } });
 });
 
 test('filter: $nin operator (comma separated)', t => {
   const res = aqp('key!=a,b');
-  t.ok(res);
-  t.same(res.filter, { key: { $nin: ['a', 'b'] } });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key: { $nin: ['a', 'b'] } });
 });
 
 test('filter: $exists operator', t => {
   const res = aqp('key1&!key2');
-  t.ok(res);
-  t.same(res.filter, { key1: { $exists: true }, key2: { $exists: false } });
+  t.truthy(res);
+  t.deepEqual(res.filter, { key1: { $exists: true }, key2: { $exists: false } });
 });
 
 test('filter: ignore default keys', t => {
   const res = aqp('key=value&skip=0&limit=10&fields=id,name&sort=name');
-  t.ok(res);
-  t.ok(res.filter);
-  t.notOk(res.filter.skip);
-  t.notOk(res.filter.limit);
-  t.notOk(res.filter.fields);
-  t.notOk(res.filter.sort);
-  t.same(res.filter, { key: 'value' });
+  t.truthy(res);
+  t.truthy(res.filter);
+  t.falsy(res.filter.skip);
+  t.falsy(res.filter.limit);
+  t.falsy(res.filter.fields);
+  t.falsy(res.filter.sort);
+  t.deepEqual(res.filter, { key: 'value' });
 });
 
 test('filter: ignore custom keys', t => {
@@ -131,120 +131,120 @@ test('filter: ignore custom keys', t => {
     projectionKey: '$fields',
     sortKey: '$sort',
   });
-  t.ok(res);
-  t.ok(res.filter);
-  t.notOk(res.filter.$skip);
-  t.notOk(res.filter.$limit);
-  t.notOk(res.filter.$fields);
-  t.notOk(res.filter.$sort);
-  t.same(res.filter, { key: 'value' });
+  t.truthy(res);
+  t.truthy(res.filter);
+  t.falsy(res.filter.$skip);
+  t.falsy(res.filter.$limit);
+  t.falsy(res.filter.$fields);
+  t.falsy(res.filter.$sort);
+  t.deepEqual(res.filter, { key: 'value' });
 });
 
 test('filter: ignore blacklisted keys', t => {
   const res = aqp('key1=value1&key2=value2&key3=value3', {
     blacklist: ['key1', 'key3'],
   });
-  t.ok(res);
-  t.ok(res.filter);
-  t.notOk(res.filter.key1);
-  t.notOk(res.filter.key3);
-  t.same(res.filter, { key2: 'value2' });
+  t.truthy(res);
+  t.truthy(res.filter);
+  t.falsy(res.filter.key1);
+  t.falsy(res.filter.key3);
+  t.deepEqual(res.filter, { key2: 'value2' });
 });
 
 test('filter: ignore all but whitelisted keys', t => {
   const res = aqp('key1=value1&key2=value2&key3=value3', {
     whitelist: ['key2'],
   });
-  t.ok(res);
-  t.ok(res.filter);
-  t.notOk(res.filter.key1);
-  t.notOk(res.filter.key3);
-  t.same(res.filter, { key2: 'value2' });
+  t.truthy(res);
+  t.truthy(res.filter);
+  t.falsy(res.filter.key1);
+  t.falsy(res.filter.key3);
+  t.deepEqual(res.filter, { key2: 'value2' });
 });
 
 test('skip', t => {
   const res = aqp('skip=10');
-  t.ok(res);
+  t.truthy(res);
   t.is(res.skip, 10);
 });
 
 test('skip (custom key)', t => {
   const res = aqp('offset=10', { skipKey: 'offset' });
-  t.ok(res);
+  t.truthy(res);
   t.is(res.skip, 10);
 });
 
 test('limit', t => {
   const res = aqp('limit=10');
-  t.ok(res);
+  t.truthy(res);
   t.is(res.limit, 10);
 });
 
 test('limit (custom key)', t => {
   const res = aqp('max=10', { limitKey: 'max' });
-  t.ok(res);
+  t.truthy(res);
   t.is(res.limit, 10);
 });
 
 test('projection (includes)', t => {
   const res = aqp('fields=a,b,c');
-  t.ok(res);
-  t.same(res.projection, { a: 1, b: 1, c: 1 });
+  t.truthy(res);
+  t.deepEqual(res.projection, { a: 1, b: 1, c: 1 });
 });
 
 test('projection (includes + _id exclude)', t => {
   const res = aqp('fields=a,b,-_id');
-  t.ok(res);
-  t.same(res.projection, { a: 1, b: 1, _id: 0 });
+  t.truthy(res);
+  t.deepEqual(res.projection, { a: 1, b: 1, _id: 0 });
 });
 
 test('projection (excludes)', t => {
   const res = aqp('fields=-a,-b,-c');
-  t.ok(res);
-  t.same(res.projection, { a: 0, b: 0, c: 0 });
+  t.truthy(res);
+  t.deepEqual(res.projection, { a: 0, b: 0, c: 0 });
 });
 
 test('projection (mix of includes/excludes)', t => {
   const res = aqp('fields=a,b,-c');
-  t.ok(res);
-  t.same(res.projection, { c: 0 });
+  t.truthy(res);
+  t.deepEqual(res.projection, { c: 0 });
 });
 
 test('projection (multiple keys)', t => {
   const res = aqp('fields=a&fields=b');
-  t.ok(res);
-  t.same(res.projection, { a: 1, b: 1 });
+  t.truthy(res);
+  t.deepEqual(res.projection, { a: 1, b: 1 });
 });
 
 test('projection (custom key)', t => {
   const res = aqp('select=a,b,c', { projectionKey: 'select' });
-  t.ok(res);
-  t.same(res.projection, { a: 1, b: 1, c: 1 });
+  t.truthy(res);
+  t.deepEqual(res.projection, { a: 1, b: 1, c: 1 });
 });
 
 test('sort', t => {
   const res = aqp('sort=a,+b,-c');
-  t.ok(res);
-  t.same(res.sort, { a: 1, b: 1, c: -1 });
+  t.truthy(res);
+  t.deepEqual(res.sort, { a: 1, b: 1, c: -1 });
 });
 
 test('sort (multiple keys)', t => {
   const res = aqp('sort=a&sort=-b');
-  t.ok(res);
-  t.same(res.sort, { a: 1, b: -1 });
+  t.truthy(res);
+  t.deepEqual(res.sort, { a: 1, b: -1 });
 });
 
 test('sort (custom key)', t => {
   const res = aqp('order=a,-b', { sortKey: 'order' });
-  t.ok(res);
-  t.same(res.sort, { a: 1, b: -1 });
+  t.truthy(res);
+  t.deepEqual(res.sort, { a: 1, b: -1 });
 });
 
 test('complex response', t => {
   const res = aqp(`sort=+a,-b&skip=10&limit=50
     &fields=foo,-_id&key1=a&key2=true,c&key3=string(10)
     &key4>4&key4<=15&key5=/foo/i&key6&!key7`);
-  t.same(res, {
+  t.deepEqual(res, {
     filter: {
       key1: 'a',
       key2: { $in: [true, 'c'] },
@@ -263,7 +263,7 @@ test('complex response', t => {
 
 test('query already parsed', t => {
   const res = aqp({ key: 'foo', limit: '50' });
-  t.same(res, {
+  t.deepEqual(res, {
     filter: {
       key: 'foo',
     },
@@ -273,7 +273,7 @@ test('query already parsed', t => {
 
 test('empty query', t => {
   const res = aqp();
-  t.same(res, {
+  t.deepEqual(res, {
     filter: {},
   });
 });
