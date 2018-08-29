3b9fa30a6286efd58bfb1c23c4e88fd2cea4899d
Eric Redon <eric.redon@coox.org>
2018-08-29 12:34:45 +0200

    Uplift packaging
    - remove extra deps
    - streamline package config
    - streamline Babel config
    - explicitly publish as CommonsJS module to `dist/cjs`
    - use np for publishing to npm

--- ava.config.js
+++ ava.config.js
@@ -1,5 +1,13 @@
 export default {
   verbose: true,
+  // Do not apply project `.babelrc` to test files, use AVA’s own Babel pipeline
+  // https://github.com/avajs/ava/blob/master/docs/recipes/babel.md#make-ava-skip-your-projects-babel-options
+  babel: {
+    testOptions: {
+      babelrc: false,
+    },
+  },
+  // Apply project `.babelrc` to source files
   require: [
     '@babel/register',
   ],
--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-const test = require('ava');
-const aqp = require('../src').default;
+import test from 'ava';
+import aqp from '../src';
 
 test('filter: basic', t => {
   const res = aqp('key=value');
