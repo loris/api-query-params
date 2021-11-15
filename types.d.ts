27ae0c55b2a2f0d35f5a19f7cb8a765c1e909445
Simon Gaudek <simon.gaudek@team-con.de>
2021-11-15 12:28:44 +0100

    fix(types): Fix types, add population and enhance query type

--- types.d.ts
+++ types.d.ts
@@ -1,14 +1,24 @@
 declare module 'api-query-params' {
+  type PopulateOptions = {
+    path: string;
+    select?: any;
+  }
+
+  type Query = {
+    [key: string]: undefined | string | string[] | Query | Query[]
+  }
+
   export type AqpQuery = {
     filter: Record<string, any>;
     skip: number;
     limit: number;
     sort: Record<string, number>;
     projection: Record<string, number>;
+    population: PopulateOptions[];
   };
 
   function aqp(
-    query: string | Record<string, string>,
+    query: string | Query,
     opt?: {
       skipKey?: string;
       limitKey?: string;
